4b9cd9e339ea255fb9d6cca9d03c9418eff1eb24
william <william@etu.hesge.ch>
2023-05-15 14:37:32 +0200

    Add filters for events and places

---
+++ backend/api/filters.py
@@ -0,0 +1,20 @@
+from django_filters import rest_framework as filters
+from .models import Event, Place
+
+class EventFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains')
+    description = filters.CharFilter(lookup_expr='icontains')
+    place_name = filters.CharFilter(field_name='place__name', lookup_expr='icontains')
+
+    class Meta:
+        model = Event
+        fields = ['name', 'description', 'place_name']
+
+class PlaceFilter(filters.FilterSet):
+    name = filters.CharFilter(lookup_expr='icontains')
+    street = filters.CharFilter(lookup_expr='icontains')
+    locality = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Place
+        fields = ['name', 'street', 'locality']
