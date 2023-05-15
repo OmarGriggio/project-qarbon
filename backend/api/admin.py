332a47d6437d7a032e76e3c941946969116ce115
william <william@etu.hesge.ch>
2023-05-15 20:02:20 +0200

    Add: user custom admin fields + place

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
-from .models import Message, Event
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from .models import Message, Event, Place 
 
 @admin.register(Message)
 class ModelAdmin(admin.ModelAdmin):
@@ -13,3 +14,16 @@ class ModelAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'date', 'place', 'user')
     list_filter = ('date',)
     search_fields = ('name', 'description')
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'locality')
+    search_fields = ('name', 'locality')
+
+# Unregister the default User admin
+admin.site.unregister(User)
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('pk', 'username', 'email')
+    search_fields = ('username', 'email')
