9a6be6c06e62e1c8513667b8323fb344109f9936
william <william@etu.hesge.ch>
2023-05-15 14:35:15 +0200

    Added routes: Place, Event + refactored duplicates

--- backend/urls.py
+++ backend/urls.py
@@ -17,56 +17,20 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from .api.views import index_view, UserViewSet, GroupViewSet, MessageViewSet, EventViewSet
-from .api import views
+from api.views import index_view, UserViewSet, GroupViewSet, MessageViewSet, EventViewSet, PlaceViewSet
 
 router = routers.DefaultRouter()
 router.register('messages', MessageViewSet)
 router.register('users', UserViewSet)
 router.register('groups', GroupViewSet)
+router.register('events', EventViewSet)
+router.register('places', PlaceViewSet)
 
 urlpatterns = [
-    # http://localhost:8000/
-    path('', views.welcome_page, name='index'),
-
-    # http://localhost:8000/api/<router-viewsets>
-    #path('api/', include(router.urls)),
-    path('api/', views.api_overview, name="API"),
-
+    path('', index_view, name='index'),
+    path('api/', include(router.urls)),
     path('api/explorer/', include('rest_framework.urls', namespace='rest_framework')),
-
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
-
     path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-
-    # http://localhost:8000/api/admin/
     path('api/admin/', admin.site.urls),
-
-    # http://localhost:8000/api/event-list/
-    path('api/event-list/', views.event_list, name="Liste des évènements"),
-
-    # http://localhost:8000/api/place-list/
-    path('api/place-list/', views.place_list, name="Liste des lieux"),
-
-    # http://localhost:8000/api/place-find/
-    path('api/place-find/<str:pk>/', views.place_find, name="Détails d'un lieu"),
-
-    # http://localhost:8000/api/place-find/
-    path('api/event-find/<str:pk>/', views.event_find, name="Détails d'un event"),
-
-    # http://localhost:8000/api/event-create/
-    path('api/event-create/', views.event_create, name="Ajout d'un évènement"),
-
-    # http://localhost:8000/api/place-create/
-    path('api/place-create/', views.place_create, name="Ajout d'un lieu"),
-
-    # http://localhost:8000/api/events-find-by-place/
-    path('api/events-find-by-place/<str:pk>/', views.event_list_by_place, name="Liste des evenements d'après un lieu"),
-
-    # http://localhost:8000/api/places-find-by-event/
-    path('api/places-find-by-event/<str:pk>/', views.place_list_by_event, name="Liste des endroits ou ont (eu) lieu un événement"),
-
-
-    # 
-    path('events/', EventViewSet.as_view({'get': 'list'}), name='events'),
 ]
