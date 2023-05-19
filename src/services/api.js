b0f5a712c5606b22d9f8dc256e2d312688cd4cd2
william <william@etu.hesge.ch>
2023-05-19 17:25:05 +0200

    Credentials sent for each request

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios"
 
-export default axios.create({
+const api = axios.create({
   baseURL: "http://localhost:8000/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json"
-  }
+  },
+  withCredentials: true
 })
+
+export default api
