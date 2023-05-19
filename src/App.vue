fb4bcdaf65b26a25c10b254891ea2f21026ced5c
william <william@etu.hesge.ch>
2023-05-19 17:24:10 +0200

    Modify routes and check token expiration

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="nav">
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link :to="{ name: 'messages' }">Django Rest</router-link> |
+    <!-- <router-link :to="{ name: 'messages' }">Django Rest</router-link> | -->
     <router-link :to="{ name: 'create-place' }">Creer un endroit</router-link> |
-    <router-link :to="{ name: 'event-form' }">Ajouter un événement</router-link> |
-    
+    <router-link :to="{ name: 'event-create' }">Ajouter un événement</router-link> |
+
     <div v-if="!user"><RouterLink :to="{ name: 'login' }">Login</RouterLink> |</div>
     <div v-else>
       <p>Logged in as {{ user.username }}</p>
@@ -16,6 +16,8 @@
 
 <script>
 import authService from "../src/services/authService"
+import { LOCALSTORARGE_TOKEN_KEY } from "../src/services/authService"
+
 
 export default {
   data() {
@@ -32,8 +34,16 @@ export default {
     }
   },
   async mounted() {
-    authService.getUser()
-    // this.messages = await messageService.fetchMessages()
+    // authService
+    authService.checkTokenExpiry()
+    if (localStorage.getItem(LOCALSTORARGE_TOKEN_KEY)) {
+      try {
+        await authService.getUser()
+        // this.messages = await messageService.fetchMessages()
+      } catch (error) {
+        console.log("Error fetching user", error)
+      }
+    }
   }
 }
 </script>
