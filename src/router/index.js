54d2317b2afcce83a8690f87ea29d75529b4ed38
william <william@etu.hesge.ch>
2023-05-19 17:24:40 +0200

    Check for auth for the event creation page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import { LOCALSTORARGE_TOKEN_KEY } from "../services/authService"
 
 const routes = [
   {
@@ -7,14 +8,14 @@ const routes = [
     name: "home",
     component: HomeView
   },
-  {
-    path: "/messages",
-    name: "messages",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/MessagesView.vue")
-  },
+  // {
+  //   path: "/messages",
+  //   name: "messages",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ "../views/MessagesView.vue")
+  // },
   {
     path: "/login",
     name: "login",
@@ -31,15 +32,28 @@ const routes = [
     component: () => import("../views/CreatePlaceView.vue")
   },
   {
-    path: "/event-form",
-    name: "event-form",
-    component: () => import("../views/EventForm.vue")
+    path: "/event-create",
+    name: "event-create",
+    component: () => import("../views/EventCreate.vue"),
+    meta: { requiresAuth: true }
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth)
+  const isAuthenticated = localStorage.getItem(LOCALSTORARGE_TOKEN_KEY)
+  console.log(isAuthenticated)
+
+  if (requiresAuth && !isAuthenticated) {
+    next("/login") // or wherever you want to redirect unauthenticated users
+  } else {
+    next()
+  }
 })
 
 export default router
