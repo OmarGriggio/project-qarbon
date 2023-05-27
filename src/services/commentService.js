f0953674a2634f597ab251667318c226c80b1d60
lucafHEG <luca.furlan@hes-so.ch>
2023-05-27 10:15:58 -0700

    remove unused code

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -4,19 +4,6 @@ export default {
   fetchComments() {
     return api.get(`comments/`).then((response) => response.data)
   },
-  async postComments(placeId, userId, token, commentText) {
-    const headers = { Authorization: `Bearer ${token}` }
-    const commentData = {
-      place: placeId, 
-      user: userId, 
-      text: commentText 
-    }
-    try {
-      await api.post("comments/", commentData, { headers })
-    } catch (err) {
-      throw err.response.data
-    }
-  },
   async fetchCommentsByPlaceId(placeId) {
     return api.get(`comments/?place=${placeId}`).then((response) => response.data)
   }
