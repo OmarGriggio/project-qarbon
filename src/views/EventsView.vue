5b9e41a03aa9fce169eda9316cd0d0cc8caa72df
william <william.senn@etu.hesge.ch>
2023-05-30 11:27:10 +0200

    Homepage visual

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -138,7 +138,7 @@ export default {
   methods: {
     filterEventsDebounced: _.debounce(function () {
       this.filterEvents()
-    }, 300),
+    }, 250),
 
     getPlace(placeId) {
       return this.places.find((place) => place.id === placeId) || {}
