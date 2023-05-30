efc559575ea22beb0a5e8bcad9e6d460ac5f5df6
omarGriggio <omar.griggio@gmail.com>
2023-05-30 13:55:34 +0200

    components for inpute picture

---
+++ src/components/PictureInpute.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="Picture">
+    <img :src="src" alt="Picture" class="img-fluid" />
+    <button class="btn btn-primary" @click="browse()">Photo</button>
+    <input type="file" accept="image/*" class="invisible" ref="file" @change="change" />
+  </div>
+</template>
+<script>
+export default {
+  emits: ["input", "src"],
+  props: {
+    value: File
+  },
+  data() {
+    return {
+      file: null,
+      src: "https://www.shutterstock.com/image-vector/continuous-line-drawing-happy-cheerful-260nw-749843629.jpg"
+    }
+  },
+  methods: {
+    browse() {
+      this.$refs.file.click()
+    },
+    change(e) {
+      this.file = e.target.files[0]
+      this.$emit("input", this.file)
+      let reader = new FileReader()
+      reader.readAsDataURL(this.file)
+      reader.onload = (e) => {
+        let src = (this.src = e.target.result)
+        this.$emit("src", src)
+      }
+    }
+  }
+}
+</script>
+<style>
+.img-fluid {
+  max-width: 20%;
+}
+</style>
