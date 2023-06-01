5c5d79e1e9346904f25410be9144b46494569820
william <william.senn@etu.hesge.ch>
2023-06-01 17:14:14 +0200

    Add : Handle wrong username or password errors

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -25,9 +25,12 @@
           </div>
           <br />
           <button type="button" class="btn btn-success col-lg" @click="login">Sign in</button>
+          <div v-if="loginError" class="alert alert-danger" role="alert">
+            <p>Wrong username or password</p>
+          </div>
         </div>
         <br />
-        <p style="padding-bottom: 30px;" v-if="!user">
+        <p style="padding-bottom: 30px" v-if="!user">
           No account ?
           <router-link style="color: var(--main-color)" to="/register">Sign up</router-link>
         </p>
@@ -49,7 +52,7 @@ export default {
     return {
       username: "",
       password: "",
-      error: "",
+      loginError: "",
       hasAccount: true
     }
   },
@@ -61,16 +64,17 @@ export default {
   methods: {
     async login() {
       this.loginError = ""
-      await authService
-        .login({
+      try {
+        await authService.login({
           username: this.username,
           password: this.password
         })
-        .catch((err) => {
-          this.loginError = err.response && err.response.data ? err.response.data : err
-          console.error(this.loginError)
-        })
-      this.$router.push("/event-list")
+        // if the login is successful, redirect to the event list
+        this.$router.push("/event-list")
+      } catch (err) {
+        this.loginError = err.response.data ? err.response.data : err.message
+        console.error(this.loginError)
+      }
     }
   },
   async mounted() {
