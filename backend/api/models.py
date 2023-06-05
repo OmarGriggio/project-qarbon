3bbef8c820643eb9f24f61e166a8d96b78401a15
lucafHEG <luca.furlan@hes-so.ch>
2023-06-05 02:46:47 -0700

    add 2 fields to the messagesusers models

--- backend/api/models.py
+++ backend/api/models.py
@@ -101,10 +101,11 @@ class MessagesUsers(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    sender_deleted = models.BooleanField(default=False)
+    receiver_deleted = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['timestamp']
 
     def __str__(self):
         return f'{self.sender.username} to {self.receiver.username}'
-    
